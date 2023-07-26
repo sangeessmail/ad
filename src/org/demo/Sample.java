4af2ab99e42db29a4a05d2234ccc9b2565377aa4
sangeessmail <sangeessmail@gmail.com>
2023-07-26 12:34:37 +0530

    msg 4

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -6,6 +6,10 @@ public class Sample {
 		System.out.println("message 1");
 
 	}
+	private void m4() {
+		System.out.println("msg4");
+
+	}
 public static void main(String[] args) {
 	System.out.println("1st code added ");
 }
