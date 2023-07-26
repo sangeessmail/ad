6234bedcd75bf83b5adde8b6eeea68fc534c79ad
sangeessmail <sangeessmail@gmail.com>
2023-07-26 11:25:56 +0530

    anu code added

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -1,6 +1,9 @@
 package org.demo;
 
 public class Sample {
+	private void m2() {
+		System.out.println("message 2");
+	}
 public static void main(String[] args) {
 	System.out.println("1st code added ");
 }
